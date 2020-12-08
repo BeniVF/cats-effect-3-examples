24704650fa5e1fb29e959023ea508ce98c3b5760
BeniVF <beni.villa@gmail.com>
2020-12-08 11:06:50 +0100

    Implement parTraverse

---
+++ core/src/main/scala/io/benivf/exercises/Main.scala
@@ -0,0 +1,17 @@
+package io.benivf.exercises
+
+import cats.effect.IOApp
+import cats.effect.IO
+import scala.concurrent.duration._
+import cats.syntax.all._
+
+object Main extends IOApp {
+  val max: Int = 10
+  import exercises.io._
+
+  def run(args: List[String]): IO[Int] =
+    parTraverse((1 to 10).toList)(x => simulateTask(x, ((max - x)*100).millis)) >>= { x =>
+      IO(println(x)).as(0)
+    }
+
+}
--- core/src/main/scala/io/benivf/exercises/exercises.scala
+++ core/src/main/scala/io/benivf/exercises/exercises.scala
@@ -5,11 +5,27 @@ import cats.syntax.all._
 import scala.concurrent.duration._
 import java.util.concurrent.TimeoutException
 import cats.effect.kernel._
+import java.time.Instant
+import cats.Show
 
 object exercises {
 
   object io {
     import cats.effect.IO
+    def time: IO[Instant] = IO(Instant.now)
+    def putStrLn[A: Show](a: A): IO[Unit] = time >>= { current =>
+      IO {
+        val name = Thread.currentThread().getName()
+        println(s"[${current.toEpochMilli()}:$name] ${a.show}")
+      }
+
+    }
+
+    def simulateTask(id: Int, time: FiniteDuration): IO[String] =
+      putStrLn(s"Starting $id (expected time $time)") >>
+        IO.sleep(time) >>
+        putStrLn(s"Done $id").as(id.show)
+
     def timeout[A](io: IO[A], duration: FiniteDuration): IO[A] =
       IO.race(
         IO.sleep(duration)
@@ -20,6 +36,12 @@ object exercises {
           ),
         io
       ) >>= (IO.fromEither)
+
+    def parTraverse[A, B](as: List[A])(f: A => IO[B]): IO[List[B]] =
+      as.foldRight(IO.pure(List.empty[B])) { case (n, acc) =>
+        IO.both(acc, f(n)).map { case (xs, x) => xs.+:(x) }
+      }
+
   }
 
   def timeout[F[_]: Concurrent[*[_], Throwable]: Temporal[*[_], Throwable], A](
