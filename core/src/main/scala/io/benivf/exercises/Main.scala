e4862526a42fe796d1e25e9834fde2d484818588
BeniVF <beni.villa@gmail.com>
2020-12-09 09:34:44 +0100

    Use bracket api

--- core/src/main/scala/io/benivf/exercises/Main.scala
+++ core/src/main/scala/io/benivf/exercises/Main.scala
@@ -12,13 +12,15 @@ object Main extends IOApp {
   def run(args: List[String]): IO[Int] =
     for {
       _ <- putStrLn(s"Create semaphore")
-      s <- Semaphore(2)
+      s <- Semaphore(1)
       result <- parTraverse((1 to 10).toList) { x =>
-        s.acquire >>
+        s.acquire.bracket { _ =>
           putStrLn(s"Acquire $x") >>
-          simulateTask(x, ((max - x) * 100).millis) >>
+            simulateTask(x, ((max - x) * 100).millis)
+        } { _ =>
           s.release >>
-          putStrLn(s"Release $x").as(x)
+            putStrLn(s"Release $x")
+        }
       }
       _ <- putStrLn(s"Done! [${result.mkString(",")}]")
     } yield 0
